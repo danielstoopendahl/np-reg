162dba8ef5c23b5cd66daf6fbced9fb3fde62c3b
danielstoopendahl <daniel.stoopendahl@gmail.com>
2026-04-01 09:27:39 +0200

    put all experiments in same file

--- image/reg_experiment.py
+++ image/reg_experiment.py
@@ -7,18 +7,26 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset, random_split
 
 HIDDEN_DIM = 16384 # [256, 512, 1024, 2048, 4096, 8192, 16384]
-BATCH_SIZE = 128 # [64, 128, 256, 512]
-REG_LAMBDA = 1 # [1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1, 3, 10]
+BATCH_SIZE = 128 # [128, 256, 512]
+NP_REG_LAMBDA = 0 # [1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1, 3, 10]
+O_REG_LAMBDA = 0
+WEIGHT_DECAY=0
+DROPOUT=0
+BATCH_NORM=False
 
-# python np_reg.py --reg-lambda=3e-4 --batch-size=64
+# python np_reg.py --o-reg-lambda=3e-5 --batch-size=128 | tee out4.txt; python np_reg.py --o-reg-lambda=3e-5 --batch-size=256 | tee out5.txt; python np_reg.py --o-reg-lambda=3e-5 --batch-size=512 | tee out6.txt;
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch-size", type=int, default=BATCH_SIZE)
-    parser.add_argument("--reg-lambda", type=float, default=REG_LAMBDA)
+    parser.add_argument("--np-reg-lambda", type=float, default=NP_REG_LAMBDA)
+    parser.add_argument("--o-reg-lambda", type=float, default=O_REG_LAMBDA)
+    parser.add_argument("--weight-decay", type=float, default=WEIGHT_DECAY)
+    parser.add_argument("--dropout", type=float, default=DROPOUT)
+    parser.add_argument("--batch-norm", action="store_true", default=BATCH_NORM)
     return parser.parse_args()
 
-def normperserving_regularization(data, features, reg_lambda):
+def normperserving_regularization(data, features, np_reg_lambda):
     """
     Computes the norm-preserving regularization penalty.
     Penalizes differences between the norm of input data and the norm of output features.
@@ -28,11 +36,23 @@ def normperserving_regularization(data, features, reg_lambda):
     features_norm = torch.norm(features.view(features.size(0), -1), p='fro', dim=1)
     norm_diff_loss = F.mse_loss(data_norm, features_norm)
     
-    return reg_lambda * norm_diff_loss
+    return np_reg_lambda * norm_diff_loss
+
+def orthogonal_regularization(weight, o_reg_lambda):
+    """
+    Computes the orthogonal regularization penalty: 
+    L = lambda * ||W^T W - I||_F^2
+    """
+
+    sym = torch.mm(weight.t(), weight)
+    identity = torch.eye(sym.size(0), device=weight.device)
+    loss_ortho = torch.norm(sym - identity, p='fro')**2
+    
+    return o_reg_lambda * loss_ortho
 
 
 class SLFN_CIFAR(nn.Module):
-    def __init__(self, hidden_dim):
+    def __init__(self, hidden_dim, dropout, use_batch_norm):
         super(SLFN_CIFAR, self).__init__()
 
         input_dim = 3 * 32 * 32
@@ -40,12 +60,18 @@ class SLFN_CIFAR(nn.Module):
 
         self.first_linear = nn.Linear(input_dim, hidden_dim)
         self.non_linear = nn.ReLU()
+        self.drop = nn.Dropout(p=dropout)
         self.second_linear = nn.Linear(hidden_dim, output_dim)
+        self.use_batch_norm = use_batch_norm
+        self.batch_norm = nn.BatchNorm1d(hidden_dim)
 
     def forward_features(self, x):
         x = torch.flatten(x, 1)
         x = self.first_linear(x)
         x = self.non_linear(x)
+        if self.use_batch_norm:
+            x = self.batch_norm(x)
+        x = self.drop(x)
         return x
 
     def forward(self, x):
@@ -54,7 +80,7 @@ class SLFN_CIFAR(nn.Module):
         return x
 
 
-def train(model, device, train_loader, optimizer, epoch, reg_lambda):
+def train(model, device, train_loader, optimizer, epoch, np_reg_lambda, o_reg_lambda):
     model.train()
     running_loss = 0.0
 
@@ -64,10 +90,11 @@ def train(model, device, train_loader, optimizer, epoch, reg_lambda):
         
         optimizer.zero_grad()
         features = model.forward_features(data)
-        normperserving_loss = normperserving_regularization(data, features, reg_lambda)
+        normperserving_loss = normperserving_regularization(data, features, np_reg_lambda)
+        orthogonal_loss = orthogonal_regularization(model.first_linear.weight, o_reg_lambda)
         
         logits = model.second_linear(features)
-        loss = F.cross_entropy(logits, target) + normperserving_loss
+        loss = F.cross_entropy(logits, target) + normperserving_loss + orthogonal_loss
         loss.backward()
         optimizer.step()
 
@@ -128,7 +155,7 @@ def main():
 
     train_subset, val_subset = random_split(
         full_train_dataset,
-        [len(full_train_dataset) - 10000, 10000],
+        [len(full_train_dataset) - 5000, 5000],
         generator=torch.Generator().manual_seed(42),
     )
     val_subset = Subset(val_base_dataset, val_subset.indices)
@@ -136,9 +163,9 @@ def main():
     train_loader = DataLoader(train_subset, batch_size=args.batch_size, shuffle=True, num_workers=12)
     val_loader = DataLoader(val_subset, batch_size=256, shuffle=False, num_workers=12)
 
-    model = SLFN_CIFAR(HIDDEN_DIM).to(device)
+    model = SLFN_CIFAR(HIDDEN_DIM, args.dropout, args.batch_norm).to(device)
 
-    optimizer = optim.Adam(model.parameters(), lr=3e-4)
+    optimizer = optim.Adam(model.parameters(), lr=3e-4, weight_decay=args.weight_decay)
     min_lr = 1e-8
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer,
@@ -152,7 +179,7 @@ def main():
     best_accuracy = 0.0
     
     for epoch in range(1,1001):
-        train_loss = train(model, device, train_loader, optimizer, epoch, args.reg_lambda)
+        train_loss = train(model, device, train_loader, optimizer, epoch, args.np_reg_lambda, args.o_reg_lambda)
 
         print(f"Epoch {epoch}: Train loss {train_loss:.6f}")
         val_loss, accuracy = test(model, device, val_loader)
@@ -171,7 +198,11 @@ def main():
 
     print(
         f"Run finished with arguments: batch_size={args.batch_size}, "
-        f"reg_lambda={args.reg_lambda}"
+        f"np_reg_lambda={args.np_reg_lambda}"
+        f"o_reg_lambda={args.o_reg_lambda}"
+        f"weight_degay={args.weight_decay}"
+        f"dropout={args.dropout}"
+        f"batchnorm={args.batch_norm}"
     )
     print(f"Best val loss: {best_val_loss:.6f}")
     print(f"Best val accuracy: {best_accuracy:.2f}%")
